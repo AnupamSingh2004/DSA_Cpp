617291a42edf1a23c11563c1d1df1e823cf248f3
AnupamSingh2004 <sanupam2004@gmail.com>
2025-06-09 19:45:27 +0530

    Updated Doubly Linked List

--- Doubly_Linked_List.cpp
+++ Doubly_Linked_List.cpp
@@ -40,11 +40,91 @@ Node* convertToDLL(vector<int>& arr){
     return head;
 }
 
+Node* deleteHead(Node* head){
+    if(head == NULL || head->next == NULL){
+        return NULL;
+    }
+    Node* prev = head;
+    head = head->next;
+    head->back = NULL;
+    prev->next = NULL;
+    delete prev;
+
+    return head;
+}
+
+
+Node* deleteTail(Node* head){
+    if(head == NULL || head->next == NULL){
+        return NULL;
+    }
+
+    Node* tail = head;
+
+    while(tail->next != NULL){
+        tail = tail->next;
+    }
+
+    Node* prev = tail->back;
+    prev->next = NULL;
+    tail->next = NULL;
+
+    delete tail;
+
+    return head;
+}
+
+
+Node* removeKthElement(Node* head,int k){
+    if(head == NULL) return NULL;
+
+    int count = 0;
+    Node* kNode = head;
+
+    while(kNode != NULL){
+        count++;
+        if(count == k) break;
+        kNode = kNode->next;
+    }
+
+    Node* prev = kNode->back;
+    Node* front = kNode->next;
+
+    if(prev == NULL && front == NULL){
+        return NULL;
+    }else if(prev == NULL){
+        return deleteHead(head);
+    }else if(front == NULL){
+        return deleteTail(head);
+    }
+
+    prev->next = front;
+    front->back = prev;
+    kNode->next = NULL;
+    kNode->back = NULL;
+
+    delete kNode;
+
+    return head;
+}
+
+void printList(Node* head){
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    
+}
+
 int main(){
 
      vector<int> arr = {2, 5, 8, 10};
 
     Node* head = convertToDLL(arr);
 
+    head = removeKthElement(head,3);
+
+    printList(head);
     return 0;
 }
